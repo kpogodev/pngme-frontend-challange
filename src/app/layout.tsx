146c81bf69a9f79e96df13821071c690f5d8c7c0
Krzysztof Pogorzelski <krzysztof.pogorzelski@autoprotectgroup.co.uk>
2024-09-20 01:06:36 +0100

    [pngme-1]: Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import '@/styles/globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Pngme - Frontend Challange',
+  description: 'Frontend Challange',
+  icons: [{ rel: 'icon', url: '/favicon.ico' }],
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
