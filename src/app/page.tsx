ca7fbf055e0857aca48eeea7b160318d0cb64ce0
Krzysztof Pogorzelski <krzysztof.pogorzelski@autoprotectgroup.co.uk>
2024-09-20 05:32:12 +0100

    [pngme-5]: Adding ui elements that are meant to be statically generated & bringing dot pattern from magicui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import { DotPattern } from '@/components/magicui/dot-pattern'
+import { cn } from '@/lib/utils'
+
 export default function Home() {
-  return <div></div>
+  return (
+    <main className="container grid place-items-center flex-1 relative z-0">
+      <div className="flex flex-col items-center gap-2">
+        <h1 className="text-6xl font-extrabold text-center">Stock Price Viewer</h1>
+        <p className="text-lg text-muted-foreground text-center">Search and view real-time stock price information</p>
+      </div>
+      <DotPattern
+        width={32}
+        height={32}
+        cr={2}
+        className={cn('[mask-image:radial-gradient(300px_circle_at_center,white,transparent)]', '-z-10 opacity-70')}
+      />
+    </main>
+  )
 }
